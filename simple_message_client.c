19f7f1d594d7d97c85c1ea50040add7e87bfc417
claudiabaierl <claudia.baierl@technikum-wien.at>
2015-11-25 10:47:05 +0100

    includes

--- simple_message_client.c
+++ simple_message_client.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+
+
+
+
+
+
+}
