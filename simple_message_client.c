6704e08aa2d62efc822b64122f7cef925660d733
claudiabaierl <claudia.baierl@technikum-wien.at>
2015-11-25 10:56:39 +0100

    verbindungsaufbau

--- simple_message_client.c
+++ simple_message_client.c
@@ -10,6 +10,17 @@
 int main(int argc, char *argv[])
 {
 
+	struct addrinfo client_info;
+	struct addrinfo *set_info, *rp;
+	size_t length;
+
+
+	memset(&client_info, 0, sizeof(struct addrinfo));
+	client_info.ai_family = AF_UNSPEC;
+	client_info.ai_socktype = SOCK_STREAM;
+	client_info.ai_protocol = 0;
+
+
 
 
 
