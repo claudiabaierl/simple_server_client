906640e3b880bf56e1b2f6c450f8a3ba8ed39c59
claudiabaierl <claudia.baierl@technikum-wien.at>
2015-11-25 14:10:05 +0100

    socket

--- simple_message_client.c
+++ simple_message_client.c
@@ -13,6 +13,8 @@ int main(int argc, char *argv[])
 	struct addrinfo client_info;
 	struct addrinfo *set_info, *rp;
 	size_t length;
+	int addr_info;
+	int socket_desc;
 
 
 	memset(&client_info, 0, sizeof(struct addrinfo));
@@ -20,6 +22,24 @@ int main(int argc, char *argv[])
 	client_info.ai_socktype = SOCK_STREAM;
 	client_info.ai_protocol = 0;
 
+	add_info = getaddrinfo(argv[1], argv[2], &client_info, &set_info);
+	if(s != 0)
+	{
+		fprintf(stderr, "getaddrinfo failed: %s\n", gai_sterror(s));
+		exit(EXIT_FAILURE);
+	}
+
+	for (rp = set_info; rp != NULL, rp = rp->ai_next)
+	{
+		sockt_desc = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+		if (sock_desc == -1)
+		{
+			continue;
+		}
+		if(connect(socket_desc, rp->ai_addr, rp->ai_addrlen) != -1)
+			break;
+		close(sock_desc);
+	}
 
 
 
