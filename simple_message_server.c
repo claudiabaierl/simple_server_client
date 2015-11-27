8be3fb623ec7f3ecb84d64b68ac279b304f2ebbd
claudiabaierl <claudia.baierl@technikum-wien.at>
2015-11-27 17:33:12 +0100

    server started

--- simple_message_server.c
+++ simple_message_server.c
@@ -1,2 +1,67 @@
-#include <stdio.h>
+/**
+ * @file simple_message_server.c
+ *
+ * VCS TCP/IP Client
+ *
+ * @author: Claudia Baierl - ic14b003 <ic14b003@technikum-wien.at>
+ * @author: Zübide Sayici - ic14b002 <ic14b002@technikum-wien.at>
+ *
+ * @version $Revision: xxx $
+ *
+ * Last Modified: $Author: xxxxx $
+ */
+
+/*
+ * ----------------------------- includes -------------------------
+ */
+
+#include <arpa/inet.h>
 #include <errno.h>
+#include <limits.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <limits.h>
+#include "simple_message_client_commandline_handling.h"
+#include <sys/wait.h>
+#include <signal.h>
+#include <getopt.h>
+
+/*
+ * ---------------------------------- globals ------------------------
+ */
+
+const char *prg_name;
+
+/*
+ * ---------------------------------- function prototypes ------------
+ */
+
+
+/**
+ *
+ * \brief Main function implements a server which connects to a client
+ * Main entry point
+ *
+ * \param argc passes the number of arguments
+ * \param argv passes the arguments (programme name is argv[0]
+ *
+ * \return EXIT_SUCCESS when no error occurred
+ * \return EXIT_FAILURE when an error occurred
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+
+
+	prg_name = argv[0];
+
+
+
+}
