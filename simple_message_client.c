a6a54f4134d437ef7dcd170b7655bb4bec4a2296
claudiabaierl <claudia.baierl@technikum-wien.at>
2015-11-28 12:50:21 +0100

    bug fixing

--- simple_message_client.c
+++ simple_message_client.c
@@ -23,6 +23,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <stdarg.h>
 #include "simple_message_client_commandline_handling.h"
 
 /*
@@ -63,7 +66,7 @@ int main(int argc, char *argv[])
 
 	struct addrinfo client_info;
 	struct addrinfo *set_info, *rp;
-	size_t length;
+	//size_t length;
 	int check;
 	int socket_desc;
 	int connect_socket;
@@ -73,10 +76,10 @@ int main(int argc, char *argv[])
 	const char *message = NULL;
 	const char *image = NULL;
 
-	prg_name = argv[0];
+
 
 	smc_parsecommandline(argc, argv, &usage, &server, &port, &user, &message, &image, &verbose);
-
+	prg_name = argv[0];
 
 	memset(&client_info, 0, sizeof(client_info));
 	client_info.ai_family = AF_UNSPEC;
@@ -86,12 +89,12 @@ int main(int argc, char *argv[])
 	check = getaddrinfo(server, port, &client_info, &set_info);
 	if(check != 0)
 	{
-		fprintf(stderr, "%s: getaddrinfo failed: %s\n", prg_name, gai_sterror(check));
+		fprintf(stderr, "%s: getaddrinfo failed: %s\n", prg_name, gai_strerror(check));
 		return EXIT_FAILURE;
 	}
 
 	/* go through all he results and connect if possible - if not, try the next one */
-	for (rp = set_info; rp != NULL, rp = rp->ai_next)
+	for (rp = set_info; rp != NULL; rp = rp->ai_next)
 	{
 		socket_desc = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
 		if (socket_desc == -1)
@@ -114,11 +117,11 @@ int main(int argc, char *argv[])
 	if(rp == NULL)
 	{
 		fprintf(stderr, "%s: could not connect to any address.\n", prg_name);
-		freeaddrinfo(client_info);
+		freeaddrinfo(set_info);
 		return EXIT_FAILURE;
 	}
 	/* is no longer needed */
-	freeaddrinfo(client_info);
+	freeaddrinfo(set_info);
 
 
 
@@ -162,7 +165,7 @@ void logger(char *message)
 	int j = 0;
 	if (verbose == TRUE)
 	{
-		j = fprintf(stdout, "%s: %s\n", prg, message);
+		j = fprintf(stdout, "%s: %s\n", prg_name, message);
 
 		/* if printig to stdout fails, a negative value is returned */
 		if(j < 0)
